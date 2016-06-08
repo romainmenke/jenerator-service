fedafaba57c08c3ee829b94024cf20466ff61601
romainmenke <romainmenke@gmail.com>
2016-06-08 23:53:10 +0200

    (update) package name

--- Package.swift
+++ Package.swift
@@ -1,6 +1,6 @@
 import PackageDescription
 let package = Package(
-    name: "jenerator-service",
+    name: "JeneratorService",
     dependencies: [
         .Package(url: "https://github.com/ibm-bluemix-mobile-services/bluemix-simple-http-client-swift.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/czechboy0/Jay.git", majorVersion: 0)
